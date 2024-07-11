e08a3e98a3e450b2c76095b757b2aec6478a598e
HBhawe <harshalb97@outlook.com>
2024-07-11 10:37:23 +0200

    Updated styles and event listeners

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const resultDiv = document.querySelector("#display");
 const p = document.createElement("p");
 
+// event listener for the clear button
 const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", getValue);
+
+// event listener for the equals button
 const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener("click", getValue);
+
+const functionButtons = document.querySelectorAll(".function");
+// use the spread to convert into an array (not necessary)
+const functionArray = [...functionButtons];
+
+functionArray.forEach(function (currentBtn) {
+  currentBtn.addEventListener("click", getValue);
+});
 
 // select all inputs
 const inputs = document.querySelectorAll(".number");
+
+// use the spread to convert into an array (not necessary)
+const inputArray = [...inputs];
+
+inputArray.forEach(function (currentBtn) {
+  currentBtn.addEventListener("click", getValue);
+});
+
+function getValue(e) {
+  console.log(e.target.textContent);
+}
 
 const operatorValue = document.querySelectorAll(".function");
 
