dc751863308d6a3872312ed14a917984b840b2a2
HBhawe <harshalb97@outlook.com>
2024-07-16 15:31:30 +0200

    updates to javascript

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+var operationObject = new Object();
+
+var number1, number2, operator, test;
+
 // selector to display the result in a "p" element
 const resultDiv = document.querySelector("#display");
 const p = document.createElement("p");
 
 // event listener for the clear button
 const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", getValue);
+clearButton.addEventListener("click", clear);
 
 // event listener for the equals button
 const equalsButton = document.querySelector(".equals");
-equalsButton.addEventListener("click", getValue);
+equalsButton.addEventListener("click", () => {
+  num1 = parseInt(operationObject.number1);
+  num2 = parseInt(operationObject.number2);
+  operator = operationObject.operator;
+  operate(num1, operator, num2);
+});
 
 const functionButtons = document.querySelectorAll(".function");
 // use the spread to convert into an array (not necessary)
@@ -28,20 +37,23 @@ inputArray.forEach(function (currentBtn) {
   currentBtn.addEventListener("click", getValue);
 });
 
-var operationObject = new Object();
-
-var number1, number2, operator, test;
-
 function getValue(e) {
   if (e.target.classList.contains("number")) {
-    if (number1 === undefined) {
+    if (number1 === undefined || operator === undefined) {
       number1 = e.target.textContent;
       p.innerText = number1;
-      operationObject.number1 = number1;
-    } else {
+      if (operationObject.number1 === undefined) {
+        operationObject.number1 = "";
+      }
+      operationObject.number1 = "" + operationObject.number1 + number1;
+    } else if (number2 === undefined) {
       number2 = e.target.textContent;
       p.innerText = number2;
-      operationObject.number2 = number2;
+      if (operationObject.number2 === undefined) {
+        operationObject.number2 = "";
+      }
+      operationObject.number2 = operationObject.number2 + number2;
+      // operationObject.number2 = number2;
     }
     resultDiv.appendChild(p);
   } else if (e.target.classList.contains("function")) {
@@ -57,7 +69,7 @@ function operate(num1, operator, num2) {
     result = add(num1, num2);
   } else if (operator === "-") {
     result = subtract(num1, num2);
-  } else if (operator === "*") {
+  } else if (operator === "X") {
     result = multiply(num1, num2);
   } else if (operator === "/") {
     result = divide(num1, num2);
@@ -82,4 +94,8 @@ function divide(num1, num2) {
   if (num1 === 0 || num2 === 0) {
     return "Not a number";
   } else return num1 / num2;
+}
+
+function clear() {
+  location.reload();
 }
